5cc99614c100d8df16160a65dbbab2930fdd01d0
Roman Czerwinski <roman.czerwinski@zoro.com>
2020-05-22 20:04:37 -0500

    initial commit!

---
+++ README.md
@@ -0,0 +1,6 @@
+# jb = JailBreak
+I have some old iPhones and am working on jailbreaking them and turning them into nodes on my in-home kubernetes cluster which currently consists of an old dell tower running Debian and a Raspberry Pi 3B+ on Raspbian. These iPhones be on some BSD Darwin shit tho. And now I need to learn some tings about Cydia, Theos, Objective-C, and probably other random shit just to get a compile installed on these bitches to build docekr and k8s from source (if it were only that simple XD)
+
+In this repos I will chronicle my journey. Hopefully I can get it working if not learn something along the way. Without further ado.
+
+[Here we go](https://media.giphy.com/media/145hX7QVWqyili/giphy.gif)
---
+++ get-started.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# echo 'Use `passwd` to change the default password for the users: ["mobile", "root"], the default is "alpine"'
+# echo ''
+
+# still nope
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python3 get-pip.py 
+
+## perhaps this for arm64?
+## open https://docs.docker.com/engine/install/debian/
+## The answer is no, the linux and unix os's are not the same, and there is no unix arm64 install for docker
+
+# maybe this but idk
+# open https://fosskb.in/2017/07/03/installing-qemu-on-freebsd-11/
+
+# building qemu from git
+# open https://www.qemu.org/download/#source
+
+# But you need the c complier to make it work :(
+# which may be gui install only. yuck.
+# Compiling C/C++
+# open https://neotan.github.io/building-vim-on-idevices/
+# Getting the equivalent of apt-get install build-essential
+# open https://neotan.github.io/building-vim-on-idevices/
+
+# you can add to cydia sources from cli using `a2cy`
+# make sure you restart the springboard `killall -9 backboard` after installs i guess
+
+# PTL There's a wiki for getting started!!!
+open http://iphonedevwiki.net/index.php/Main_Page
+# and a jailbreak specific subreddit
+open https://www.reddit.com/r/jailbreakdevelopers/
+
